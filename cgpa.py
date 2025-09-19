eaf844369c77c68f1227d7cd8e95cf6abb219491
sriramvissakoti <153540774+Ram6023@users.noreply.github.com>
2025-09-19 12:36:03 +0530

    cgpa.py

--- cgpa.py
+++ cgpa.py
@@ -1,17 +1,18 @@
 import streamlit as st
 
-st.title("CGPA ↔ Percentage Calculator")
+st.title("CGPA & Percentage Calculator")
 
-option = st.radio("Choose Conversion:", ["CGPA ➝ Percentage", "Percentage ➝ CGPA"])
+option = st.radio("Choose Conversion:", ["CGPA to Percentage", "Percentage to CGPA"])
 
-if option == "CGPA ➝ Percentage":
+if option == "CGPA to Percentage":
     cgpa = st.number_input("Enter your CGPA", min_value=0.0, max_value=10.0, step=0.01)
     if st.button("Convert to Percentage"):
         percentage = cgpa * 9.5
         st.success(f"Your Percentage is: {percentage:.2f}%")
 
-elif option == "Percentage ➝ CGPA":
+elif option == "Percentage to CGPA":
     percentage = st.number_input("Enter your Percentage", min_value=0.0, max_value=100.0, step=0.1)
     if st.button("Convert to CGPA"):
         cgpa = percentage / 9.5
         st.success(f"Your CGPA is: {cgpa:.2f}")
+
